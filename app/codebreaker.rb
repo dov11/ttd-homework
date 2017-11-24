69ac73efa0f6121aeb4a7e30cecd0b374a37ae1a
dov11 <ovo.dan@gmail.com>
2017-11-24 21:09:37 +0100

    pass until G

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -14,10 +14,30 @@ class Codebreaker
     end
 
     def guess(input)
-      # Make sure to replace next line with the actual implemented marking algorithm,
-      # using the @secret_number
-
-      output.puts "you typed '#{input}'"
+      output.puts answer(input)
     end
+    def answer(input)
+      return "Try guessing a number with four digits" if input.length != 4
+      answer = ['']
+      number_of_exact_matches(input).times {|i| answer<<"+"}
+      number_of_misplaced_matches(input).times {|i| answer<<"-"}
+      answer.join('')
+    end
+    def number_of_exact_matches(input)
+      i_array = input.split('')
+      i_array
+      .select{|element| i_array.index(element)==secret_array.index(element)}
+      .length
+    end
+    def number_of_misplaced_matches(input)
+      i_array = input.split('')
+      i_array
+      .select{|element| i_array.index(element)!=secret_array.index(element) && @secret_number.include?(element)}
+      .length
+    end
+    def secret_array
+      @secret_number.split('')
+    end
+
   end
 end
